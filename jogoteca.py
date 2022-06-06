7fd1b4f68b0c4be703b6763aa4e255d78c3e1b44
Jonatas <jonatasfontele@gmail.com>
2022-06-06 16:40:41 -0300

    feat: New game form created

--- jogoteca.py
+++ jogoteca.py
@@ -20,5 +20,10 @@ def hello_world():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
 app.run()
 # app.run(host='0.0.0.0', port=8080)
